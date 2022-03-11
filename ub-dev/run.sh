9173bf7e5de4ebaf43055832036895e1db985569
Thomux <me@homux.eu>
2022-03-11 17:59:41 +0100

    fix Linux scripts

--- ub-dev/run.sh
+++ ub-dev/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir -p ./ub-dev
+mkdir -p ~/ub-dev
 
-docker run -it --rm -v docker run -it --rm -v "$(pwd)"/ub-dev:/home/tom/work -v ~/.ssh:/home/tom/.ssh:ro  -v ~:/home/tom/home:ro ub-dev zsh
+docker run -it --rm -v ~/ub-dev:/home/tom/work -v ~/.ssh:/home/tom/.ssh:ro  -v ~:/home/tom/host:ro ub-dev zsh
