d7dd6652d98407d9f1e7457bd88e040382d82af3
Thomux <me@thomxu.eu>
2022-03-11 17:18:06 +0100

    fix yocto

--- yocto/run.bat
+++ yocto/run.bat
@@ -1,2 +1,3 @@
-mkdir c:\work
-docker run -it --rm -v c:\work:/home/tom/work -v %homedrive%%homepath%\.ssh:/home/tom/.ssh:ro -v %homedrive%%homepath%:/home/tom/host:ro yocto-honister bash
+mkdir c:\out
+docker volume create yocto-honister
+docker run -it --rm -v c:\out:/home/tom/out -v yocto-honister:/home/tom/work -v %homedrive%%homepath%\.ssh:/home/tom/.ssh:ro -v %homedrive%%homepath%:/home/tom/host:ro yocto-honister bash
