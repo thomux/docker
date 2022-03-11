3e432442d0e6ce45dcaf0508b6263d5ff6f13f62
Thomux <me@thomxu.eu>
2022-03-11 12:43:32 +0100

    Yocto container

---
+++ yocto/run.bat
@@ -0,0 +1,2 @@
+mkdir -p ./work
+docker run -it --rm -v work:/home/tom/work yocto-honister bash
---
+++ yocto/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p ./work
+
+docker run -it --rm -v work:/home/tom/work yocto-honister bash
